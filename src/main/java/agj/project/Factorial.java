dc1353eac2dc44e68407b0cdefd3032948480eb3
andres <andresgj@uma.es>
2017-10-31 20:07:02 +0100

    New factorizacion

--- src/main/java/agj/project/Factorial.java
+++ src/main/java/agj/project/Factorial.java
@@ -1,22 +1,22 @@
 package agj.project;
 
-
-public class Factorial 
-{
+public class Factorial {
 
 	public long compute(long value) {
-		if (value < 0 ) {
-			throw new RuntimeException ("The value is negative" + value);
-			
+		long result;
+		if (value < 0) {
+			throw new RuntimeException("The value is negative" + value);
+
 		}
-		if ((value==0) || (value == 1)){
-			return 1;
-		} else if (value==2 ) {
-					return 2;
+		if ((value == 0) || (value == 1)) {
+			result = 1;
+		} else if (value == 2) {
+			result = 2;
 		} else {
-			return value*compute(value-1);
+			result = value * compute(value - 1);
 		}
+		return result;
+	}
+
 	
-	}  
- 
 }
